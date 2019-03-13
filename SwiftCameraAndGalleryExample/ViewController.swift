84b52e1f70c02e4a82d6df4e47ec921a3a591ae7
moxDroid Labs Inc <pritam.world@gmail.com>
2019-03-13 19:55:03 -0400

    Open Camera and Photo Library code added

--- SwiftCameraAndGalleryExample/ViewController.swift
+++ SwiftCameraAndGalleryExample/ViewController.swift
@@ -4,25 +4,57 @@
 //
 //  Created by Pritesh Patel on 2019-03-13.
 //  Copyright © 2019 Pritesh Patel. All rights reserved.
-//
+//https://hackernoon.com/swift-access-ios-camera-and-photo-library-dc1dbe0cdd76
+//https://www.ioscreator.com/tutorials/take-photo-ios-tutorial
 
 import UIKit
 
-class ViewController: UIViewController
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate
 {
     @IBOutlet weak var imgPhoto: UIImageView!
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     @IBAction func btnGalleryClick(_ sender: UIBarButtonItem)
     {
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary)
+        {
+            let myPickerController = UIImagePickerController()
+            myPickerController.delegate = self;
+            myPickerController.sourceType = .photoLibrary
+            self.present(myPickerController, animated: true, completion: nil)
+        }
     }
     
     @IBAction func btnCameraClick(_ sender: UIBarButtonItem)
     {
+        if UIImagePickerController.isSourceTypeAvailable(.camera)
+        {
+            let myPickerController = UIImagePickerController()
+            myPickerController.delegate = self;
+            myPickerController.sourceType = .camera
+            self.present(myPickerController, animated: true, completion: nil)
+        }
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            self.imgPhoto.image = image
+        }else{
+            print("Something went wrong")
+        }
+        
+        for (k, _) in info {
+            print("TEST - \(k)")
+        }
+        self.dismiss(animated: true, completion: nil)
     }
 }
 
