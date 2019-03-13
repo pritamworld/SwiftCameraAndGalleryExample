10046e64793bb06e65816c7fd00c5ab2131b5c57
MacStudent <macstudent@126-Instructor.local>
2019-03-13 18:00:49 -0400

    Initial Commit

---
+++ SwiftCameraAndGalleryExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftCameraAndGalleryExample
+//
+//  Created by MacStudent on 2019-03-13.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
