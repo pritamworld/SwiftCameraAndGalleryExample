b7f92847f90643b768e60ad1453a38713e4fce48
moxDroid Labs Inc <pritam.world@gmail.com>
2019-03-13 20:02:23 -0400

    Save Image to library code added

--- SwiftCameraAndGalleryExample/ViewController.swift
+++ SwiftCameraAndGalleryExample/ViewController.swift
@@ -8,6 +8,7 @@
 //https://www.ioscreator.com/tutorials/take-photo-ios-tutorial
 
 import UIKit
+import MobileCoreServices
 
 class ViewController: UIViewController
 {
@@ -26,7 +27,10 @@ class ViewController: UIViewController
             let myPickerController = UIImagePickerController()
             myPickerController.delegate = self;
             myPickerController.sourceType = .photoLibrary
+            myPickerController.mediaTypes = [kUTTypeImage as String]
+            myPickerController.allowsEditing = false
             self.present(myPickerController, animated: true, completion: nil)
+            newMedia = false
         }
     }
     
@@ -37,7 +41,10 @@ class ViewController: UIViewController
             let myPickerController = UIImagePickerController()
             myPickerController.delegate = self;
             myPickerController.sourceType = .camera
+            myPickerController.mediaTypes = [kUTTypeImage as String]
+            myPickerController.allowsEditing = false
             self.present(myPickerController, animated: true, completion: nil)
+            newMedia = true
         }
     }
     
@@ -51,16 +58,42 @@ extension ViewController:  UIImagePickerControllerDelegate, UINavigationControll
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        let mediaType = info[UIImagePickerController.InfoKey.mediaType] as! NSString
+        
         if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
             self.imgPhoto.image = image
+            //Save Image if taken from Camera
+            if (newMedia == true) {
+                UIImageWriteToSavedPhotosAlbum(image, self, #selector(self.image(image:didFinishSavingWithError:contextInfo:)), nil)
+            } else if mediaType.isEqual(to: kUTTypeMovie as String) {
+                // Code to support video here
+            }
         }else{
             print("Something went wrong")
         }
         
+        //Display all keys
         for (k, _) in info {
             print("TEST - \(k)")
         }
         self.dismiss(animated: true, completion: nil)
+    }
+    
+    @objc func image(image: UIImage, didFinishSavingWithError error: NSErrorPointer, contextInfo:UnsafeRawPointer) {
+        
+        if error != nil {
+            let alert = UIAlertController(title: "Save Failed",
+                                          message: "Failed to save image",
+                                          preferredStyle: UIAlertController.Style.alert)
+            
+            let cancelAction = UIAlertAction(title: "OK",
+                                             style: .cancel, handler: nil)
+            
+            alert.addAction(cancelAction)
+            self.present(alert, animated: true,
+                         completion: nil)
+        }
     }
 }
 
